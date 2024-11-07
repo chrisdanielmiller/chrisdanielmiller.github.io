c90f0b09395b0c607814fbb4e8d622cb2ef781b8
chrisdanielmiller <chrisdanielmiller@gmail.com>
2024-11-06 19:28:40 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -138,7 +138,7 @@
 					hideOnSwipe: true,
 					resetScroll: true,
 					resetForms: true,
-					side: 'left',
+					side: 'right',
 					target: $body,
 					visibleClass: 'header-visible'
 				});
